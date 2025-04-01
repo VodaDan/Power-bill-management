98c6e43e3c270c19e66249ae181c8097320f7ba2
Dan <voda.daniel98@yahoo.com>
2025-04-01 23:05:20 +0300

    Configured gRPC , created a gRPC request to create a bill successful

---
+++ customer-service/src/main/java/com/pm/customerservice/proto/BillingGrpcServiceClient.java
@@ -0,0 +1,30 @@
+package com.pm.customerservice.proto;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BillingGrpcServiceClient {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcServiceClient.class);
+    private final billing.BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingGrpcServiceClient (@Value ("${billing.service.address}")String serverAddress, @Value("${billing.service.grpc.port}") int serverPort) {
+    log.info("Billing service channel is being initialized at {}:{}",serverAddress,serverPort);
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress,serverPort).usePlaintext().build();
+        blockingStub = billing.BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+    public billing.BillingResponse createBilling(String customerId, String amount) {
+        billing.BillingRequest billingRequest = billing.BillingRequest.newBuilder().setAmount(amount).setCustomerId(customerId).build();
+        billing.BillingResponse billingResponse = blockingStub.createBilling(billingRequest);
+        log.info("Received billing response from gRPC service: {}",billingResponse );
+
+        return  billingResponse;
+    }
+
+
+}
