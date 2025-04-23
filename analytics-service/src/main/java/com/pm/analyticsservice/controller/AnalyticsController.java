f31fdfe35c9db2da54591df65fd9f9c2e59323c7
Dan <voda.daniel98@yahoo.com>
2025-04-23 17:21:57 +0300

    feat: added rest endpoint for generating analytics

--- analytics-service/src/main/java/com/pm/analyticsservice/controller/AnalyticsController.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/controller/AnalyticsController.java
@@ -1,20 +1,29 @@
 package com.pm.analyticsservice.controller;
 
+import com.pm.analyticsservice.Service.AnalyticsService;
 import com.pm.analyticsservice.dto.AnalyticsResponseDTO;
 import com.pm.analyticsservice.dto.CustomerAnalyticsResponseDTO;
 import com.pm.analyticsservice.model.CustomerAnalytics;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/analytics")
 public class AnalyticsController {
 
+    private final AnalyticsService analyticsService;
 
-    @GetMapping("/analytics")
+    public AnalyticsController(AnalyticsService analyticsService) {
+        this.analyticsService = analyticsService;
+    }
+
+    @GetMapping
     public ResponseEntity<AnalyticsResponseDTO> getAnalytics(){
-        
+        AnalyticsResponseDTO analyticsResponseDTO = analyticsService.generateAnalytics();
+        return  ResponseEntity.ok().body(analyticsResponseDTO);
     }
 
 }
