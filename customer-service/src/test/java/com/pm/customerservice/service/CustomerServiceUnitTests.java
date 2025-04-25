aeda7696820af5e57965d51ad071bc32813e8850
Dan <voda.daniel98@yahoo.com>
2025-04-25 15:59:39 +0300

    test: added tests for create/get customers from service package

--- customer-service/src/test/java/com/pm/customerservice/service/CustomerServiceUnitTests.java
+++ customer-service/src/test/java/com/pm/customerservice/service/CustomerServiceUnitTests.java
@@ -1,16 +1,17 @@
 package com.pm.customerservice.service;
 
+import com.google.protobuf.Any;
 import com.pm.customerservice.dto.CustomerRequestDTO;
 import com.pm.customerservice.dto.CustomerResponseDTO;
 import com.pm.customerservice.exception.AddressAlreadyExistsException;
+import com.pm.customerservice.kafka.KafkaProducer;
 import com.pm.customerservice.mapper.CustomerMapper;
 import com.pm.customerservice.module.Customer;
 import com.pm.customerservice.repository.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
@@ -18,12 +19,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -35,9 +36,15 @@ public class CustomerServiceUnitTests {
     @Mock
     private CustomerRepository customerRepository;
 
+
+    @Mock
+    private KafkaProducer kafkaProducer;
+
     @InjectMocks
     private CustomerService customerService;
 
+
+    // Update Customers Tests
     @Test
     public void CustomerServiceUpdateCustomerReturnsUpdated () {
         Customer customer = createMockCustomer();
@@ -80,6 +87,81 @@ public class CustomerServiceUnitTests {
     }
 
 
+    // Get Customers Tests
+    @Test
+    public void CustomerServiceGetCustomerReturnsList() {
+        Customer mockCustomer = createMockCustomer();
+        Customer mockCustomer2 = createMockCustomer();
+        when(customerRepository.findAll()).thenReturn(List.of(mockCustomer, mockCustomer2));
+        List<CustomerResponseDTO> list = customerService.getCustomers();
+
+        assertNotNull(list);
+        assertEquals(2, list.size());
+    }
+
+    @Test
+    public void CustomerServiceGetCustomerReturnsEmptyWhenNoCustomers() {
+        when(customerRepository.findAll()).thenReturn(Collections.emptyList());
+        List<CustomerResponseDTO> list = customerService.getCustomers();
+
+        assertTrue(list.isEmpty());
+        assertEquals(0, list.size());
+
+    }
+
+    // Create Customer Tests
+
+    @Test
+    public void CustomerServiceCreateCustomerReturnsCustomerResponseDTO() {
+
+        Customer customerModel = createMockCustomer();
+        CustomerRequestDTO customer = CustomerMapper.toRequest(customerModel);
+        when(customerRepository.existsByAddress(customer.getAddress())).thenReturn(false);
+        when(customerRepository.save(any(Customer.class))).thenReturn(customerModel);
+        CustomerResponseDTO response = customerService.createCustomer(customer);
+
+        assertNotNull(response);
+        assertEquals(CustomerResponseDTO.class,response.getClass());
+        assertEquals(response.getName(), customer.getName());
+        assertEquals(response.getAddress(), customer.getAddress());
+        assertEquals(response.getEmail(),customer.getEmail());
+
+    }
+
+    @Test
+    public void CustomerServiceCreateCustomerThrowsExceptionIfAddressExists() {
+        Customer customerModel = createMockCustomer();
+        CustomerRequestDTO customer = CustomerMapper.toRequest(customerModel);
+        when(customerRepository.existsByAddress(customer.getAddress())).thenReturn(true);
+
+        AddressAlreadyExistsException exception = assertThrows(AddressAlreadyExistsException.class, () -> customerService.createCustomer(customer));
+
+        assertEquals("Address '"+ customer.getAddress() + "' is already registered in our system !", exception.getMessage());
+    }
+
+    @Captor
+    private ArgumentCaptor<Customer> customerCaptor;
+
+    @Test
+    public void CustomerServiceCreateCustomerSendEventKafka() {
+        Customer customerModel = createMockCustomer();
+        CustomerRequestDTO customer = CustomerMapper.toRequest(customerModel);
+
+        when(customerRepository.existsByAddress(customer.getAddress())).thenReturn(false);
+        when(customerRepository.save(any(Customer.class))).thenReturn(customerModel);
+
+
+
+        customerService.createCustomer(customer);
+        verify(kafkaProducer).sendCreateEvent(customerCaptor.capture());
+        Customer capturedCustomer = customerCaptor.getValue();
+
+        verify(kafkaProducer, times(1)).sendCreateEvent(capturedCustomer);
+        assertEquals(capturedCustomer.getAddress(), customerModel.getAddress());
+    }
+
+
+    // Mocks
 
     private static int addressCounter = 0;
 
