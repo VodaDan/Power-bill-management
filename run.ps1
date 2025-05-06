fd3311609806f134bbe5587b6b7bfc104aac8792
Dan <voda.daniel98@yahoo.com>
2025-05-06 20:14:47 +0300

    fix: small fix for api-gateway route validation

--- run.ps1
+++ run.ps1
@@ -2,4 +2,4 @@ Write-Host "Stopping and removing old containers..."
 docker compose down --remove-orphans
 
 Write-Host "Building and starting fresh..."
-docker compose up --build
+docker compose up --build  --force-recreate
